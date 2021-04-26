a0ef43048b57e4b0cf43ae682f9a80f37430283e
senaygui <senaygeletaw@gmail.com>
2021-04-26 13:48:46 +0300

    template setup

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,41 +1,9 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require jquery-migrate-3.0.1.min
-//= require popper.min
-//= require bootstrap.min
-//= require jquery.easing.1.3
-//= require jquery.waypoints.min
-//= require jquery.stellar.min
-//= require owl.carousel.min
-//= require jquery.magnific-popup.min
-//= require aos
-//= require jquery.animateNumber.min
-//= require bootstrap-datepicker
-//= require scrollax.min
-//= require google-map
+ AOS.init({
+ 	duration: 800,
+ 	easing: 'slide'
+ });
 
-
-$(document).on('turbolinks:load', function(){
-	AOS.init({
-	 	duration: 800,
-	 	easing: 'slide'
- 	});
-
-
+(function($) {
 
 	"use strict";
 
@@ -375,4 +343,8 @@ $(document).on('turbolinks:load', function(){
 	  'autoclose': true
 	});
 
-})
+
+
+
+})(jQuery);
+
