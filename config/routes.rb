429f233529806c7a4f068a809e8b2c3758c06029
senaygui <senaygeletaw@gmail.com>
2021-05-07 11:35:29 +0300

    add destination

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :pages, only: [:create]
   resources :tours, only: [:index,:show]
   resources :books
+  resources :destinations, only: [:index,:show]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
